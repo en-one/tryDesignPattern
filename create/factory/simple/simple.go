2867cbc9ead13a2a1d9589888228d4c17f92c68e
zhangliyu <zhangliyu@shining3d.com>
2024-06-20 19:48:12 +0800

    feat: 更新工厂方法模式

--- create/factory/simple/simple.go
+++ create/factory/simple/simple.go
@@ -1,30 +1,53 @@
 package simple
 
-import "fmt"
-
-type Fruit interface {
-	Introduce(name string) string
+// 水果接口
+type IFruit interface {
+	setName(name string)
+	getName() string
 }
 
-type Apple struct{}
-
-func (Apple) Introduce(name string) string {
-	return fmt.Sprintf("i was an %s", name)
+// 具体水果类
+type Fruit struct {
+	name string
 }
 
-type Banana struct{}
+func (f *Fruit) setName(name string) {
+	f.name = name
+}
+func (f *Fruit) getName() string {
+	return f.name
+}
 
-func (Banana) Introduce(name string) string {
-	return fmt.Sprintf("i was a %s", name)
+// 具体水果-----苹果
+type Apple struct{ Fruit }
+
+func newApple() IFruit {
+	return &Apple{
+		Fruit: Fruit{name: "apple"},
+	}
+}
+
+// 具体水果-----香蕉
+type Banana struct{ Fruit }
+
+func newBanana() IFruit {
+	return &Banana{
+		Fruit: Fruit{name: "banana"},
+	}
+}
+
+// 重写香蕉的名字
+func (b *Banana) getName() string {
+	return b.name + "rewrite"
 }
 
 // NewFruitFactory 当我们以NewXXX的方式创建对象/接口的时候，当返回为接口的时候，就是简单工厂模式
-func NewFruit(t string) Fruit {
+func NewFruit(t string) IFruit {
 	switch t {
 	case "Apple":
-		return Apple{}
+		return newApple()
 	case "Banana":
-		return Banana{}
+		return newBanana()
 	}
 	return nil
 }
--- create/factory/simple/simple_test.go
+++ create/factory/simple/simple_test.go
@@ -1,35 +1,14 @@
 package simple
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 )
 
 func TestNewFruitFactory(t *testing.T) {
-	type args struct {
-		t string
-	}
-	tests := []struct {
-		name string
-		args args
-		want Fruit
-	}{
-		{
-			name: "Apple",
-			args: args{t: "Apple"},
-			want: Apple{},
-		},
-		{
-			name: "Banana",
-			args: args{t: "Banana"},
-			want: Banana{},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewFruit(tt.args.t); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewFruitFactory() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	banana := NewFruit("Banana")
+	fmt.Println(banana.getName())
+
+	apple := NewFruit("Apple")
+	fmt.Println(apple.getName())
 }
