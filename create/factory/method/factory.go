9204ae68848ca84f4e19734efe9dc02ea358dca0
zhangliyu <zhangliyu@shining3d.com>
2024-07-07 15:16:45 +0800

    feat: 工厂模式更新

--- create/factory/method/factory.go
+++ create/factory/method/factory.go
@@ -1,5 +1,17 @@
 package simple
 
+/*
+
+如何实现简单工厂模式
+	1、 Go 本身是没有构造函数的，一般而言我们采用 NewName  的方式创建对象/接口，当它返回的是接口的时候，其实就是简单工厂模式
+
+如何实现工厂方法模式：
+	1、构建一个接口IFruit
+	2、构建一个结构体Fruit，实现接口IFruit
+	3、构建工厂的结构体Apple，Banana。结构体中包含第二步中的结构体Fruit；同时每个工厂结构体可以重写接口中方法
+	4、写一个new函数，返回IFruit。根据传入的参数，返回不同的结构体
+*/
+
 // 水果接口
 type IFruit interface {
 	setName(name string)
